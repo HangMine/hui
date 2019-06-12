501bffc5b9f77bc09bad213aa5d6a0ac311732a2
HangMine <95629358@qq.com>
2019-06-12 11:17:00 +0800

    最新版本

--- src/main.js
+++ src/main.js
@@ -1,28 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-import HUI from './hui'
+import HUI from "./hui";
 Vue.use(HUI);
 
-import '@/assets/font/iconfont.css';
+import "@/assets/font/iconfont.css";
 
-import VueLazyLoad from 'vue-lazyload'
+import VueLazyLoad from "vue-lazyload";
 Vue.use(VueLazyLoad);
 
-import FastClick from 'fastclick'
-FastClick.attach(document.body)
+import FastClick from "fastclick";
+FastClick.attach(document.body);
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
 
 //引入全局demo-header
 import DemoHeader from "./views/DemoHeader.vue";
-Vue.component('demo-header', DemoHeader);
+Vue.component("demo-header", DemoHeader);
 new Vue({
   router,
   store,
   render: h => h(App),
-
-}).$mount('#app')
+}).$mount("#app");
